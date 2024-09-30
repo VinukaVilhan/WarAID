7fb21ea3f74335dba11981a5737754c1da3301f4
VinukaVilhan <fernandopullevv.22@uom.lk>
2024-09-30 20:25:33 +0530

    news

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ChatBot from './pages/chatBotPage';
 import Home from './pages/homePage';
 import AudioTranscriptionPage from './pages/audioTransciptionPage';
+import NewsPage from './pages/newsPage';
 
 function AppRouter() {
   return (
@@ -10,6 +11,8 @@ function AppRouter() {
         <Route path="/" element={<Home />} />
         <Route path="/ChatBot" element={<ChatBot />} />
         <Route path="/AudioTranscription" element={<AudioTranscriptionPage />} />
+        <Route path="/News" element={<NewsPage />}/>
+        
       </Routes>
     </Router>
   );

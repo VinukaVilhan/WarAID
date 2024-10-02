a28c1618d0316edbae1c6100945d16127ad143cd
VinukaVilhan <fernandopullevv.22@uom.lk>
2024-10-02 11:48:09 +0530

    navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,10 +1,10 @@
-// src/components/Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuthContext } from '@asgardeo/auth-react';
 
 const Navbar = () => {
   const { state, signIn, signOut } = useAuthContext();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <nav className="flex items-center justify-between flex-wrap bg-blue-700 p-6">
@@ -15,40 +15,43 @@ const Navbar = () => {
         <span className="font-semibold text-xl tracking-tight">War Aid App</span>
       </div>
       <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-blue-200 border-blue-400 hover:text-white hover:border-white">
+        <button 
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="flex items-center px-3 py-2 border rounded text-blue-200 border-blue-400 hover:text-white hover:border-white"
+        >
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
           </svg>
         </button>
       </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div className={`w-full flex-grow lg:flex lg:items-center lg:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
         <div className="text-sm lg:flex-grow">
-          <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">
+          <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">
             Homepage
-          </a>
-          <a href="/ResourceLocator" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">
+          </Link>
+          <Link to="/ResourceLocator" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">
             Resource Locator
-          </a>
-          <a href="/Documentation" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">
+          </Link>
+          <Link to="/Documentation" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">
             Documentation Tool
-          </a>
-          <a href="/ChatBot" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">
+          </Link>
+          <Link to="/ChatBot" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white">
             Chatbot
-          </a>
+          </Link>
         </div>
-        <div>
+        <div className="mt-4 lg:mt-0">
           {state?.isAuthenticated ? (
             <button
               onClick={() => signOut()}
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0"
+              className="w-full lg:w-auto inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white"
             >
               Logout
             </button>
           ) : (
             <button
               onClick={() => signIn()}
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0"
+              className="w-full lg:w-auto inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white"
             >
               Login / Signup
             </button>
